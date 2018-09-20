29b147ae9f6542f262ced9fc5e68bb4cea5fdca8
lpatel <lipsa.patel@netscout.com>
2018-09-20 16:27:53 -0500

    Algorithms

---
+++ src/HeapSort.java
@@ -0,0 +1,104 @@
+import java.util.Arrays;
+
+/**
+ * Heap Sort - Java Implementation
+ *
+ * What is Heap Sort?
+ *
+ * 1) Heap Sort is a comparison based sorting algorithm
+ * 2) Heap Sort is considered as improved selection sort, it divides the input into sorted and unsorted region.
+ * 3) The improvement from selection sort is to use Heap Data Structure instead of using linear search algorithm to reduce time complexity
+ *
+ * What is Binary Heap?
+ *
+ * Heap is a tree based data structure which satisfies two properties
+ *
+ * Shape Property:
+ * Heap data structure is always a complete binary tree. The complete binary tree is a binary tree which is completely filled
+ * (means all the nodes has 2 children) except the last level which not be completely full.
+ *
+ * Heap Property: All nodes are either greater than equal to (Max-Heap) or less than equal to (Min-Heap) than each of it's child nodes.
+ * This is called heap property.
+ *
+ * How Heap Sort Algorithm work?
+ *
+ * Sorting in ascending order:
+ *
+ * 1) Create a max heap from the given input array
+ * 2) Extract the root (it will be the maximum value in array) and replace it with the last element in the array.
+ * 3) Again Heapify the root (create max heap on reduced heap)
+ * 4) Repeat step 2 and 3 until entire array is sorted.
+ *
+ * Sorting in descending order:
+ *
+ * 1) Create min Heap from the given input array
+ * 2) Extract root ( it will the minimum value in array) and replace it with the last element in the array
+ * 3) Heapify the root of the heap with reduce size
+ * 4) Repeat the steps 2 and 3 till entire array is sorted
+ *
+ * Time Complexity: O(nlogn)
+ *
+ */
+public class HeapSort {
+
+    private static void heapSort(int[] array) {
+
+        int size = array.length;
+
+        //Build max heap
+        for (int i = size/2 - 1; i >= 0; i--) {
+
+            heapify(array, size, i);
+        }
+
+        //Extract the max and move it to the last location and call heapify (create max heap) on reduced heap
+        for (int i = size - 1; i >= 0; i--) {
+
+            int temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+
+            //Call heapify on the reduced heap (create max heap out of reduced heap)
+            heapify(array, i, 0);
+        }
+    }
+
+    //Heapify - max heap with node i
+    private static void heapify(int[] array, int heapSize, int i) {
+
+        int largestElementPosition = i;
+        int leftChildPosition = 2 * i + 1;
+        int rightChildPosition = 2 * i + 2;
+
+        //If left child is larger than root
+        if (leftChildPosition < heapSize && array[leftChildPosition] > array[largestElementPosition]) {
+            largestElementPosition = leftChildPosition;
+        }
+
+        //If right child is larger than largest element
+        if (rightChildPosition < heapSize && array[rightChildPosition] > array[largestElementPosition]) {
+            largestElementPosition = rightChildPosition;
+        }
+
+        if (largestElementPosition != i) {
+
+            //Swap both of them
+            int temp = array[i];
+            array[i] = array[largestElementPosition];
+            array[largestElementPosition] = temp;
+
+            //Make Recursive call to heapify for the subtree
+            heapify(array, heapSize, largestElementPosition);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] array = {9, 10, 5, 3, 1, 2, 6};
+
+        System.out.println("Original array is: " + Arrays.toString(array));
+        heapSort(array);
+
+        System.out.println("Sorted array is (Heap Sort): " + Arrays.toString(array));
+    }
+}
