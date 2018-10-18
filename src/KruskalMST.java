9bd211e559d23e04ef1d3f9e6c3999488d5e8c1c
lpatel <lipsa.patel@netscout.com>
2018-10-18 16:40:51 -0500

    Algorithms

--- src/KruskalMST.java
+++ src/KruskalMST.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 /**
  * Kruskal's Algorithm - Minimum Spanning Tree (MST)
  *
@@ -5,6 +9,7 @@
  *
  * 1) Kruskal's algorithm for finding the Minimum Spanning Tree (MST), which finds an edge of the least possible
  * weight that connects any two trees in the forest.
+ * What this means is all vertices are connected by edge and if any edge is broken
  * 2) It is a greedy algorithm.
  * 3) It finds a subset of the edges that forms a tree that includes every vertex, hwere the total weight of all the edges in the tree
  * is minimized.
@@ -37,5 +42,104 @@ public class KruskalMST {
         }
     }
 
+    private static int vertices;
+    private static ArrayList<Edge> allEdges = new ArrayList<>();
 
+    private static void initializeGraph(int nodes) {
+        vertices = nodes;
+    }
+
+    private static void addEdge(int source, int destination, int weight) {
+
+        Edge edge = new Edge(source, destination, weight);
+        allEdges.add(edge);
+    }
+
+    private static void kruskalMST() {
+
+        //Sort the edges by weight using priority queue
+        PriorityQueue<Edge> pq = new PriorityQueue<>(allEdges.size(), new Comparator<Edge>() {
+
+            @Override
+            public int compare(Edge edge1, Edge edge2) {
+                return edge1.weight - edge2.weight;
+            }
+        });
+
+        //Add all the edges to priority queue
+        for (int i = 0; i < allEdges.size(); i++) {
+            pq.add(allEdges.get(i));
+        }
+
+        //Create parent
+        int[] parent = new int[vertices];
+
+        makeSet(parent);
+
+        ArrayList<Edge> mst = new ArrayList<>();
+
+        //Process vertices
+        for (int i = 0; i < allEdges.size(); i++) {
+
+            Edge edge = pq.remove();
+
+            //Find parent
+            int x_set = findParent(parent, edge.source);
+            int y_set = findParent(parent, edge.destination);
+
+            if (x_set == y_set) {
+                //Cycle so ignore this edge
+            } else {
+                //Add to mst
+                mst.add(edge);
+
+                //Do union - Make x as parent of y
+                parent[y_set] = x_set;
+            }
+        }
+
+        //Print MST
+        System.out.println("Minimum Spanning Tree: ");
+        printGraph(mst);
+    }
+
+    private static void makeSet(int[] parent) {
+        //Make set - create a new element with a parent pointer to itself.
+        for (int i = 0; i < vertices; i++) {
+            parent[i] = i;
+        }
+    }
+
+    private static int findParent(int[] parent, int vertex) {
+
+        //Chain of parent pointers from vertex to upwards through the tree until
+        //an element is reached whose parent is itself
+        if (parent[vertex] != vertex) {
+            return findParent(parent, parent[vertex]);
+        }
+
+        return vertex;
+    }
+
+    private static void printGraph(ArrayList<Edge> edgeList) {
+
+        for (int i = 0; i < edgeList.size(); i++) {
+            Edge edge = edgeList.get(i);
+
+            System.out.println("Edge - " + i + " source: " + edge.source + " destination: " + edge.destination + " weight: " + edge.weight);
+        }
+    }
+
+    public static void main(String[] args) {
+        int vertices = 6;
+        initializeGraph(vertices);
+        addEdge(0, 1, 4);
+        addEdge(0, 2, 3);
+        addEdge(1, 2, 1);
+        addEdge(1, 3, 2);
+        addEdge(2, 3, 4);
+        addEdge(3, 4, 2);
+        addEdge(4, 5, 6);
+        kruskalMST();
+    }
 }
